bb7b568bd1a88f530d8b89c619f80b73a68e66e4
lcfxybq <lcfxybq@163.com>
2026-06-04 19:09:01 +0800

    add plant height training scripts

---
+++ plant_height/train_yolo8.py
@@ -0,0 +1,21 @@
+import os.path as osp
+from pathlib import Path
+from ultralytics import YOLO
+
+
+current_file_dir = Path(__file__).resolve().parent
+pretrain_model = osp.join(current_file_dir, Path("../data/yolov8m-pose.pt"))
+data_cfg = osp.join(current_file_dir, Path("../../data/plant_height/plant_height.yaml"))
+workspace = osp.join(current_file_dir, Path("../runs"))
+
+# Load a model
+model = YOLO(pretrain_model)
+
+# Train the model
+results = model.train(
+    data=data_cfg, 
+    epochs=100, 
+    batch=16,
+    imgsz=640,
+    project=workspace,
+    name="plant_height_yolov8",)
